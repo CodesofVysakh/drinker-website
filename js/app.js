226ed10927a6be5f9d6bd058d292c4cab86524f6
CodesofVysakh <onvysakh309@gmail.com>
2021-09-28 16:56:49 -0700

    Completed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 $(window).ready(function(){
-    $('.single-item').slick({
-        
-    });
+    $('.single-item').slick();
+
+    let menu_img = $("header div.right div.menu img");
+    let menu = $("header div.right div.menu");
+    let menu_items = $("header div.right div.menu-items");
+    let body_content = $("#body-content");
+    let midil = $("header div.right div.top-right")
+
+    $(menu_img).on("click", ()=>{
+        $(menu).toggleClass("active");
+        $(menu_items).toggleClass("active");
+        $(body_content).toggleClass("active");
+        $(midil).toggleClass("active");
+
+    })
 })
